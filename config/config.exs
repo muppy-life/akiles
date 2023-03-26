2c1465c25b483016c5b7da462efa5e2015d7e967
Borja Serra <102184302+b-serra@users.noreply.github.com>
2023-03-26 20:51:07 +0200

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,5 @@ import Config
 config :akiles,
   api_key: System.get_env("AKILES_API_KEY", nil) || raise "Missing env variable AKILES_API_KEY"
 
-import_config "#{config_env()}.exs"
+#import_config "#{config_env()}.exs"
 
