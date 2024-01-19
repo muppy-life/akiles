b436946091547715f2adc16ee0dd259edc6465e8
Borja Serra <b.serra@muppy.com>
2024-01-19 14:33:41 +0100

    elixir version upgraded to match Caronte one

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Akiles.MixProject do
     [
       app: :akiles,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env),
       deps: deps()
